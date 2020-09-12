da582a01d59162a80b149e0de4046c09d8e0b2cd
Lavkush3411 <71175721+Lavkush3411@users.noreply.github.com>
2020-09-12 16:44:36 +0530

    Add files via upload

---
+++ age calculator.py
@@ -0,0 +1,9 @@
+i=input("enter your `birthdate seprating by '-' = ")
+a=i.split("-")
+d=map(int,a)
+import datetime
+b=datetime.datetime(*d)
+c=datetime.datetime.now()
+year=(c-b)//datetime.timedelta(days=365.25)
+e=(c-b)%datetime.timedelta(days=365.25)
+print(year,"year",e)
